5d4d00ab773c3a3cc26ebd8569819c0941d29ea4
Nurzat Taalaibekova <tnurzat91@gmail.com>
2024-01-17 23:24:23 +0300

    UI styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,31 @@
-
+'use client';
+import React from "react";
+import {Grid, TextField} from "@mui/material";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function Home() {
   return (
-    <div >
-      hi
-    </div>
-  )
+    <Grid container justifyContent="space-between" alignItems="center" sx={{mb: 2}}>
+      <Grid item xs={5}>
+        <TextField label="Enter encode" variant="outlined" style={{ width: '80%', fontSize: '16px'}} />
+      </Grid>
+      <Grid item xs={2} alignItems="center">
+        <Grid item xs={10}>
+          <TextField
+          label="Enter password"
+          variant="outlined"
+          fullWidth
+        />
+        </Grid>
+        <Grid item xs={2}>
+          <ArrowBackIcon/>
+          <ArrowForwardIcon/>
+        </Grid>
+      </Grid>
+      <Grid item xs={5}>
+        <TextField label="Enter decode" variant="outlined" style={{ width: '80%', fontSize: '16px'}}/>
+      </Grid>
+    </Grid>
+  );
 }
